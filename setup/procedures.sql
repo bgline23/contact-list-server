9cab1d553160b77d5d33689e7733f87efc632456
bgline23 <nhlapobg23@gmail.com>
2022-05-22 23:53:51 +0200

    added stored procs, table updates

--- setup/procedures.sql
+++ setup/procedures.sql
@@ -1,8 +1,71 @@
 DELIMITER $$
 
+ DROP PROCEDURE IF EXISTS CreateContact;
 
-DROP PROCEDURE IF EXISTS CreateUser;
-DROP PROCEDURE IF EXISTS GetAuthenticatedUser;
+ CREATE PROCEDURE CreateContact
+(
+
+  IN `id` char(36) ,
+  IN `user_id` char(36) ,
+  IN `first_name` varchar(100) ,
+  IN `surname` varchar(100) ,
+  IN `middle_name` varchar(100) ,
+  IN `email` varchar(50) ,
+  IN `home_number` varchar(12) ,
+  IN `cell_number` varchar(12) 
+
+)
+BEGIN
+	IF first_name NOT REGEXP '[:alpha:]' 
+		OR middle_name NOT REGEXP '[:alpha:]'
+			OR surname NOT REGEXP '[:alpha:]'
+    THEN
+         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Name cannot contain a number';
+    END IF;
+    
+    INSERT INTO `epicentre`.`Contact`
+	(`id`,
+	`user_id`,
+	`first_name`,
+	`surname`,
+	`middle_name`,
+	`email`,
+	`home_number`,
+	`cell_number`)
+	VALUES
+	(	id,
+		user_id,
+		first_name,
+		surname,
+		middle_name,
+		email,
+		home_number,
+		cell_number
+	);
+END $$
+
+
+-- **************************************
+
+DROP PROCEDURE IF EXISTS GetContactList;
+
+CREATE PROCEDURE GetContactList(
+    IN user_id CHAR(36),
+    IN page_offset INT,
+    IN page_size INT
+)
+BEGIN	
+	SELECT 		first_name, 
+				surname,
+				home_number, 
+				cell_number 
+    FROM 		Contact
+    WHERE 		is_deleted = 0
+    ORDER BY 	first_name ASC
+    LIMIT 		page_offset, page_size;
+END$$
+
+-- **************************************
 
 DROP PROCEDURE IF EXISTS CreateUser;
 
@@ -27,7 +90,7 @@ BEGIN
     END IF;
 
 --  Verify fields are valid
-    IF CHAR_LENGTH(TRIM(name)) < 2 OR name REGEXP '[0-9]'
+    IF name REGEXP '[0-9]'
     THEN
          SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Name cannot contain a number';
     END IF;
@@ -76,8 +139,42 @@ BEGIN
     WHERE       u.id = LAST_INSERT_ID();
 END $$
 
+-- **************************************
+
+DROP PROCEDURE IF EXISTS CreateNote;
+
+CREATE  PROCEDURE CreateNote(
+	id char(36),
+	contact_id char(36) ,
+	title varchar(200) ,
+	description text
+)
+BEGIN
+	IF EXISTS (SELECT id
+				FROM Contact c
+                WHERE c.id = contact_id AND c.is_deleted = 0)
+	THEN
+		INSERT INTO Note
+		(
+			id,
+			contact_id,
+			title,
+			description
+		)
+		VALUES
+		(
+			id,
+			contact_id,
+			title,
+			description		
+		);
+    END IF;
+END$$
+
 
 -- **************************************
+
+DROP PROCEDURE IF EXISTS GetAuthenticatedUser;
 
 CREATE PROCEDURE GetAuthenticatedUser(IN username VARCHAR(100), IN password CHAR(32))
 BEGIN
--- setup/schema.sql
+++ setup/schema.sql
@@ -1,1 +1,37 @@
-﻿
+﻿DROP TABLE IF EXISTS User;
+CREATE TABLE `User` (
+  `id` char(36) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` char(32) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+
+DROP TABLE IF EXISTS Contact;
+CREATE TABLE `Contact` (
+  `id` char(36) NOT NULL,
+  `user_id` char(36) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `middle_name` varchar(100) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `home_number` varchar(12) NOT NULL,
+  `cell_number` varchar(12) NOT NULL,
+  `is_deleted` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`)
+) ;
+
+DROP TABLE IF EXISTS Note;
+CREATE TABLE `Note` (
+  `id` char(36) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `description` text,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `contact_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_contact_id_idx` (`contact_id`),
+  CONSTRAINT `fk_contact_id` FOREIGN KEY (`contact_id`) REFERENCES `Contact` (`id`) ON DELETE CASCADE
+) ;
